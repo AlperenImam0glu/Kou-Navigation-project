794a8c678b96a31e66a3cbef3974bbe20ea10854
AlperenImam0glu <alperenimamoglu16@gmail.com>
2022-08-02 23:12:01 +0300

    proje dosyaları duzenlendi

--- lib/core/routing_googleMap_app.dart
+++ lib/core/routing_googleMap_app.dart
@@ -4,17 +4,9 @@ class MapUtils {
   MapUtils._();
 
   static Future<void> openMap(double latitude, double longitude) async {
-    var httpsUri = Uri(
-        scheme: 'https',
-        host: 'www.google.com',
-        path: '/maps/search/?api=1&query=',
-        fragment: '$latitude,$longitude');
-    print("============================");
-    print(httpsUri);
     Uri _url = Uri.parse(
         'https://www.google.com/maps/search/?api=1&query=$latitude,$longitude');
-    print(_url);
-    print("============================");
+
     if (await canLaunchUrl(_url)) {
       await launchUrl(_url);
     } else {
