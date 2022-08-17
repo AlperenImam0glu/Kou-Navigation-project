c63a4b2d0c4adf12ed3117966a36165cf9e00365
AlperenImam0glu <alperenimamoglu16@gmail.com>
2022-08-17 12:58:46 +0300

    open street map için branch açıldı

--- lib/views/map_view.dart
+++ lib/views/map_view.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:kou_navigation_project/constant.dart';
 import 'package:kou_navigation_project/models/location_model.dart';
 import 'package:kou_navigation_project/core/routing_googlemap_app.dart';
@@ -16,16 +15,12 @@ class MapView extends StatefulWidget {
 }
 
 class MapViewState extends State<MapView> {
-  Completer<GoogleMapController> _controller = Completer();
   LocationModels locationModel;
-  List<LatLng> polylineCoordinates = [];
   Location location = new Location();
   late LocationData locationData;
   static double _startingCameraLat = 40.821772;
   static double _startingCameraLng = 29.9222003;
   static double _cameraZoom = 16;
-  static LatLng startPoint = LatLng(40.821772, 29.9222003);
-  static LatLng finishPoint = LatLng(40.824689, 29.921045);
   String progressTitle = "Konum Alınıyor";
   final String floatingActionButtoText = "Harita Uygulamasında Aç";
   static LocationData? currentLocations;
@@ -42,7 +37,7 @@ class MapViewState extends State<MapView> {
     _startingCameraLat = locationModel.lat!;
     _startingCameraLng = locationModel.lng!;
     getCurrentLocation();
-    _goSelectedLocation(lat: locationModel.lat!, lng: locationModel.lng!);
+
     mapDrawTimer();
   }
 
@@ -60,11 +55,7 @@ class MapViewState extends State<MapView> {
             textAlign: TextAlign.center,
           ),
           centerTitle: true),
-      body: currentLocations == null
-          ? isLocationEnable == true
-              ? _circularProgress()
-              : _googleMapWidgetNoLine()
-          : _googleMapWidget(),
+      body: Text("${_startingCameraLat} ${_startingCameraLng}"),
       floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
       floatingActionButton: _floatingActionButtonGoMap(),
     );
@@ -114,29 +105,6 @@ class MapViewState extends State<MapView> {
     });
   }
 
-  static final CameraPosition _statringLocation = CameraPosition(
-    target: LatLng(_startingCameraLat, _startingCameraLng),
-    zoom: _cameraZoom,
-  );
-
-  void getPolyPoints() async {
-    PolylinePoints polylinePoints = PolylinePoints();
-    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
-      google_api_key,
-      PointLatLng(startPoint.latitude, startPoint.longitude),
-      PointLatLng(finishPoint.latitude, finishPoint.longitude),
-      travelMode: TravelMode.walking,
-    );
-
-    if (result.points.isNotEmpty) {
-      result.points.forEach((PointLatLng point) =>
-          polylineCoordinates.add(LatLng(point.latitude, point.longitude)));
-    }
-    if (mounted) {
-      setState(() {});
-    }
-  }
-
   FloatingActionButton _floatingActionButtonGoMap() {
     return FloatingActionButton.extended(
       onPressed: () {
@@ -170,63 +138,5 @@ class MapViewState extends State<MapView> {
         ),
       ],
     ));
-  }
-
-  GoogleMap _googleMapWidget() {
-    return GoogleMap(
-      mapType: MapType.normal,
-      myLocationEnabled: true,
-      initialCameraPosition: _statringLocation,
-      onMapCreated: (GoogleMapController controller) {
-        startPoint =
-            LatLng(currentLocations!.latitude!, currentLocations!.longitude!);
-        finishPoint = LatLng(locationModel.lat!, locationModel.lng!);
-        if (currentLocations != null) {
-          getPolyPoints();
-        }
-        _controller.complete(controller);
-      },
-      markers: _createMarker(),
-      polylines: {
-        Polyline(
-            polylineId: PolylineId("Yol"),
-            points: polylineCoordinates,
-            color: Colors.red,
-            width: 5),
-      },
-    );
-  }
-
-  GoogleMap _googleMapWidgetNoLine() {
-    return GoogleMap(
-      mapType: MapType.normal,
-      myLocationEnabled: true,
-      initialCameraPosition: _statringLocation,
-      onMapCreated: (GoogleMapController controller) {
-        _controller.complete(controller);
-      },
-      markers: _createMarker(),
-    );
-  }
-
-  Set<Marker> _createMarker() {
-    return <Marker>[
-      Marker(
-        markerId: MarkerId("${locationModel.name}"),
-        position: LatLng(
-            locationModel.lat ?? 40.822232, locationModel.lng ?? 29.921614),
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
-      )
-    ].toSet();
-  }
-
-  Future<void> _goSelectedLocation(
-      {required double lat, required double lng}) async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(
-      CameraUpdate.newCameraPosition(
-        CameraPosition(target: LatLng(lat, lng), zoom: _cameraZoom),
-      ),
-    );
   }
 }
