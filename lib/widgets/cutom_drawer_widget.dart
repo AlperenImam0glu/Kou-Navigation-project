9ea250a73e85569b2ce540e703427f6166cd239a
AlperenImam0glu <alperenimamoglu16@gmail.com>
2022-08-23 15:54:19 +0300

    drawer içindeki linkler açılmazsa alert dialog ile geri dönüş sağlıyor

--- lib/widgets/cutom_drawer_widget.dart
+++ lib/widgets/cutom_drawer_widget.dart
@@ -12,6 +12,7 @@ class CustomDrawer extends StatelessWidget {
     final _lightColor = LightColor();
     final String kouLogoPath = "assets/icons/kou_pin_logo.png";
     final double sizedBoxHeight = 20;
+    bool isUrlOpened = true;
     return Drawer(
       backgroundColor: _lightColor.kouGreen,
       width: MediaQuery.of(context).size.width * 0.7,
@@ -39,8 +40,13 @@ class CustomDrawer extends StatelessWidget {
                       )),
                   title: Text('Web site',
                       style: Theme.of(context).textTheme.subtitle2),
-                  onTap: () {
-                    OpenUrl.openMap("https://www.kocaeli.edu.tr/");
+                  onTap: () async {
+                    isUrlOpened =
+                        await OpenUrl.openMap("https://www.kocaeli.edu.tr/");
+
+                    if (isUrlOpened == false) {
+                      showDialogs(context);
+                    }
                   },
                 ),
                 SizedBox(
@@ -52,8 +58,12 @@ class CustomDrawer extends StatelessWidget {
                       child: Image.asset("assets/icons/instagram.png")),
                   title: Text("Instagram",
                       style: Theme.of(context).textTheme.subtitle2),
-                  onTap: () {
-                    OpenUrl.openMap("https://www.instagram.com/kou92official/");
+                  onTap: () async {
+                    isUrlOpened = await OpenUrl.openMap(
+                        "https://www.instagram.com/kou92official/");
+                    if (isUrlOpened == false) {
+                      showDialogs(context);
+                    }
                   },
                 ),
                 SizedBox(
@@ -65,8 +75,13 @@ class CustomDrawer extends StatelessWidget {
                       child: Image.asset("assets/icons/facebook.png")),
                   title: Text("Facebook",
                       style: Theme.of(context).textTheme.subtitle2),
-                  onTap: () {
-                    OpenUrl.openMap("https://www.facebook.com/kou92official/");
+                  onTap: () async {
+                    isUrlOpened = await OpenUrl.openMap(
+                        "https://www.facebook.com/kou92official/");
+
+                    if (isUrlOpened == false) {
+                      showDialogs(context);
+                    }
                   },
                 ),
                 SizedBox(
@@ -78,8 +93,13 @@ class CustomDrawer extends StatelessWidget {
                       child: Image.asset("assets/icons/twitter.png")),
                   title: Text("Twitter",
                       style: Theme.of(context).textTheme.subtitle2),
-                  onTap: () {
-                    OpenUrl.openMap("https://twitter.com/kou92official");
+                  onTap: () async {
+                    isUrlOpened = await OpenUrl.openMap(
+                        "https://twitter.com/kou92official");
+
+                    if (isUrlOpened == false) {
+                      showDialogs(context);
+                    }
                   },
                 ),
                 SizedBox(
@@ -91,15 +111,49 @@ class CustomDrawer extends StatelessWidget {
                       child: Image.asset("assets/icons/youtube.png")),
                   title: Text("Youtube",
                       style: Theme.of(context).textTheme.subtitle2),
-                  onTap: () {
-                    OpenUrl.openMap(
+                  onTap: () async {
+                    isUrlOpened = await OpenUrl.openMap(
                         "https://www.youtube.com/c/kocaeli%C3%BCniversitesi");
+
+                    if (isUrlOpened == false) {
+                      showDialogs(context);
+                    }
                   },
                 ),
               ],
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  void showDialogs(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (_) => WillPopScope(
+        onWillPop: () async {
+          return true;
+        },
+        child: AlertDialog(
+          insetPadding: EdgeInsets.symmetric(horizontal: 0),
+          contentPadding: EdgeInsets.only(top: 10),
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          title: Center(child: Text("Bağlantı açılamadı")),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(10.0),
+            ),
+          ),
+          content: Icon(
+            Icons.link_off_outlined,
+            size: (MediaQuery.of(context).size.width * 0.1),
+          ),
+          actions: [
+            Center(
+                child: ElevatedButton(onPressed: () {}, child: Text("Tamam")))
+          ],
+        ),
       ),
     );
   }
